422f12400312df980f3f6beca417fc69c986824d
MichaelGallinago <michael.gallinago@gmail.com>
2025-05-22 00:22:57 +0500

    Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "net.micg.plantcare"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.24"
+        versionCode = 16
+        versionName = "1.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
